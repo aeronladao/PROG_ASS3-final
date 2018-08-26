3f4f3c0470df2a32241b17c335a8c4291f6725ea
aeronladao <42083031+aeronladao@users.noreply.github.com>
2018-08-26 16:44:04 +0300

    Add files via upload

---
+++ Arithmetic 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int one,two, three;
+    cout<< "Lets try";
+    cout<<"\n\n If I had 3 different types of  pair of shoes, how many it would be in total?\n\n";
+    cout<<"Enter three numbers to calculate its total:\n";
+    cout<<"Enter first number of pair of shoes:\n";
+    cin>>one;
+    cout<<"Enter second number of pair of shoes:\n";
+    cin>>two;
+    cout<<"Enter third number of pair of shoes:\n";
+    cin>>three;
+    cout<<"Calculate the total number of pair of shoes:\n\n";
+    cout<<one+two+three;
+    cout<<"total number of pair of shoes:\n";
+    return 0;
+}
